67e5d2b4f94fb7f9297952e8407c73828fcd2a94
Andreas Larsson <zlaktarn@gmail.com>
2018-03-19 14:13:11 +0100

    Testing

--- Masteroids/Masteroids/Game1.cs
+++ Masteroids/Masteroids/Game1.cs
@@ -6,6 +6,13 @@ namespace Masteroids {
     public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Player player1;
+        Player player2;
+        Vector2 playerPos, position;
+        bool enteredGame = false;
+        int screenWidth = 1920, screenHeight = 1080;
+        SpriteFont font;
+
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -20,6 +27,14 @@ namespace Masteroids {
 
         protected override void LoadContent() {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D playerShip = Content.Load<Texture2D>("shipTex");
+
+            //Player 1, Kontroll och Tangentbord
+            player1 = new Player(playerShip, new Vector2(screenWidth / 2, screenHeight / 2), PlayerIndex.One);
+
+            player2 = new Player(playerShip, new Vector2(200, 200), PlayerIndex.Two);
+
+            font = Content.Load<SpriteFont>("font");
 
         }
 
@@ -30,12 +45,46 @@ namespace Masteroids {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            GamePadCapabilities capabilities =
+                GamePad.GetCapabilities(PlayerIndex.Two);
+            //GamePadCapabilities capabilities = GamePad.GetCapabilities(PlayerIndex.One);
+
+            if (capabilities.IsConnected)
+            {
+                GamePadState gamePadState = GamePad.GetState(PlayerIndex.Two);
+                //if (capabilities.HasLeftXThumbStick)
+                //Player 2, Tangentboard endast
+                if (gamePadState.Buttons.Start == ButtonState.Pressed)
+                {
+                    enteredGame = true;
+                }
+            }
+
+            player1.Update(gameTime);
+            if (enteredGame)
+            {
+                player2.Update(gameTime);
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            spriteBatch.Begin();
+
+            player1.Draw(spriteBatch);
+
+            if (enteredGame)
+            {
+                player2.Draw(spriteBatch);
+            }
+            else
+            {
+                spriteBatch.DrawString(font, "Press start to Enter", new Vector2(1700, 980), Color.White);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Masteroids/Masteroids/Player.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+    public class Player
+    {
+        Texture2D texture;
+        public Vector2 position, velocity, acceleration, rotationCenter, direction, startPosition;
+        public Vector2 origin;
+        float rotation = 0.1f;
+        SpriteEffects entityFx;
+        public PlayerIndex playerValue;
+        float scale = 0.5f;
+        //velocity += forward* acceleration_amount * delta_time;
+
+        public float linearVelocity = 0.04f; //Frammåt
+        public float rotationVelocity = 3f; //Hastighet den roterar
+
+        public Player(Texture2D texture, Vector2 position, PlayerIndex playerValue)
+        {
+            this.playerValue = playerValue; ////Avgör spelare. -> återfinns på loadcontent Game1
+            this.texture = texture;
+            this.position = position;
+            startPosition = new Vector2(200, 200);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+            var direction = new Vector2((float)Math.Cos(MathHelper.ToRadians(90) - rotation), -(float)Math.Sin(MathHelper.ToRadians(90) - rotation));
+            position += velocity;
+
+            GamePadCapabilities capabilities =
+                GamePad.GetCapabilities(playerValue);
+            if (capabilities.IsConnected)
+            {
+                GamePadState gamePadState = GamePad.GetState(playerValue);
+                if (capabilities.HasLeftXThumbStick)
+                {
+                    //Rotera med styrspak
+                    if (gamePadState.ThumbSticks.Left.X < -0.1f) //0.1f står för hur mycket spaken ska luta för att svänga.
+                        rotation -= 0.07f;
+                    if (gamePadState.ThumbSticks.Left.X > 0.1f)
+                        rotation += 0.07f;
+
+                    //Frammåt och Bakåt med Knappar
+                    if (gamePadState.Buttons.A == ButtonState.Pressed)
+                        velocity += direction * linearVelocity;
+                    if (gamePadState.Buttons.B == ButtonState.Pressed) //Bakåt men är inte cannon så placerad inom //
+                        velocity -= direction * linearVelocity;
+
+
+
+                    ////Frammåt och Bakåt med Styrspak
+                    //if (gamePadState.ThumbSticks.Left.Y > 0.5f)
+                    //    position += direction * linearVelocity;
+                    //if (gamePadState.ThumbSticks.Left.Y < -0.5f)
+                    //    position -= direction * linearVelocity/3;
+                }
+            }
+
+
+            //Rotera med tangentbord
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                rotation -= MathHelper.ToRadians(rotationVelocity);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                rotation += MathHelper.ToRadians(rotationVelocity);
+
+            //Frammåt och Bakåt med tangentbord
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                position += direction * linearVelocity;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                position -= direction * linearVelocity / 3;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Vector2(position.X, position.Y), null, Color.White, rotation, new Vector2(texture.Width / 2, texture.Height - 20), scale, entityFx, 0);
+        }
+    }
+}
