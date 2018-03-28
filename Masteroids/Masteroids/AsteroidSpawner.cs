0c0afacedc45c586edd436fbc0cfe64915ab80ed
Laila <lailalaila332@hotmail.com>
2018-03-28 13:11:51 +0200

    Changed speedY in AsteroidSpawner

--- Masteroids/Masteroids/AsteroidSpawner.cs
+++ Masteroids/Masteroids/AsteroidSpawner.cs
@@ -28,7 +28,7 @@ namespace Masteroids
                 int positionX = rnd.Next(0, viewport.Width - Art.AsteroidTex.Width);
                 int positionY = viewport.Height + Art.AsteroidTex.Height;
                 int speedX = rnd.Next(-2, 2);
-                int speedY = rnd.Next(-2, 2);
+                int speedY = rnd.Next(-3, -1);
                 Vector2 position = new Vector2(positionX, positionY);
                 Vector2 speed = new Vector2(speedX, speedY);
                 entityMgr.CreateAsteroid(position, speed);
