07620ddc1bc585022f2f402f5d6f383cd5478aee
Simon <casimonni@gmail.com>
2018-04-06 19:22:34 +0200

    Added Shooter class

---
+++ Masteroids/Masteroids/Enemies/Shooter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+    class Shooter : Enemy
+    {
+        public Shooter(Vector2 position, Viewport viewport)
+            : base(position, viewport)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
