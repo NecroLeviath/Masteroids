be61e2b265f92b5b66b99dc1083da92933faf27d
Simon <casimonni@gmail.com>
2018-04-06 19:20:25 +0200

    Added Enemy class

---
+++ Masteroids/Masteroids/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+    class Enemy : GameObject
+    {
+        public Enemy(Vector2 position, Viewport viewport)
+            : base(position, viewport)
+        {
+            this.position = position;
+        }
+
+        public override void Update(GameTime gameTime) { }
+    }
+}
