f72b8ab81b13fcf708b2108856e3f6b80e15e952
y8gi <36210065+y8gi@users.noreply.github.com>
2018-04-11 13:31:48 +0200

    Menu 80%  finnish
    Button in right place,
    "New Game"
    "Highscore"
    "Quit Game"
    
    Next time: Background

--- Masteroids/Masteroids/Controls/Button.cs
+++ Masteroids/Masteroids/Controls/Button.cs
@@ -7,15 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MasteroidMenu.Controls
-{
-    class Button : Component
-    {
+namespace Masteroids.Controls {
+    public class Button : Component {
         private MouseState _currentMouse;
 
-        private SpriteFont Menufont;
+        private SpriteFont _font;
 
-        private bool MouseisHovering;
+        private bool _isHovering;
 
         private MouseState _previousMouse;
 
@@ -29,50 +27,42 @@ namespace MasteroidMenu.Controls
 
         public Vector2 Position { get; set; }
 
-        public Rectangle Rectangle
-        {
-            get
-            {
+        public Rectangle Rectangle {
+            get {
                 return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
             }
         }
 
         public string Text { get; set; }
 
-        public Button(Texture2D texture, SpriteFont font)
-        {
+        public Button(Texture2D texture, SpriteFont font) {
             _texture = texture;
-            Menufont = font;
+            _font = font;
             PenColour = Color.Black;
 
         }
 
-        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
             var colour = Color.White;
-            if (MouseisHovering)
+            if (_isHovering)
                 colour = Color.Gray;
 
             spriteBatch.Draw(_texture, Rectangle, colour);
-            if (!string.IsNullOrEmpty(Text))
-            {
-                var x = (Rectangle.X + (Rectangle.Width / 2)) - (Menufont.MeasureString(Text).X / 2);
-                var y = (Rectangle.Y + (Rectangle.Width / 2)) - (Menufont.MeasureString(Text).Y / 2);
-                spriteBatch.DrawString(Menufont, Text, new Vector2(x, y), PenColour);
+            if (!string.IsNullOrEmpty(Text)) {
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColour);
             }
         }
-        public override void Update(GameTime gameTime)
-        {
+        public override void Update(GameTime gameTime) {
             _previousMouse = _currentMouse;
             _currentMouse = Mouse.GetState();
-            var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
-            MouseisHovering = false;
+            Rectangle mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
+            _isHovering = false;
 
-            if (mouseRectangle.Intersects(Rectangle))
-            {
-                MouseisHovering = true;
-                if (_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed)
-                {
+            if (mouseRectangle.Intersects(Rectangle)) {
+                _isHovering = true;
+                if (_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed) {
                     Click.Invoke(this, new EventArgs());
                 }
             }
--- Masteroids/Masteroids/Game1.cs
+++ Masteroids/Masteroids/Game1.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Masteroids.States;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Masteroids {
- 
-
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
     public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D MasteroidMenu;
+        private State _currentstate;
+        private State _nextState;
+        public void ChangeState(State state) {
+            _nextState = state;
+        }
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -17,7 +25,7 @@ namespace Masteroids {
 
         protected override void Initialize() {
 
-
+            IsMouseVisible = true;
             base.Initialize();
         }
 
@@ -25,6 +33,8 @@ namespace Masteroids {
         protected override void LoadContent() {
 
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _currentstate = new MenuState(this, graphics.GraphicsDevice, Content);
 
 
         }
@@ -38,16 +48,21 @@ namespace Masteroids {
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-
+            if (_nextState != null) {
+                _currentstate = _nextState;
+                _nextState = null;
+            }
+            _currentstate.Update(gameTime);
+            _currentstate.PostUpdate(gameTime);
 
             base.Update(gameTime);
         }
 
+
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-
+            _currentstate.Draw(gameTime, spriteBatch);
 
             base.Draw(gameTime);
         }
