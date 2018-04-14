4b65c4d15ff76e0720829713cb528efe108fa229
Simon <casimonni@gmail.com>
2018-04-14 14:36:54 +0200

    Added support for sound effects

--- Masteroids/Masteroids/Sound.cs
+++ Masteroids/Masteroids/Sound.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,19 @@ namespace Masteroids
     static class Sound
     {
         public static Song Music { get; private set; }
+        
+        //public static SoundEffect SEffect;
+        // DEV: Or for sound effects with variation
+        //private static Random rand = new Random();
+        //private static SoundEffect[] sEffects;
+        //public static SoundEffect SEffect { get { return sEffects[rand.Next(sEffects.Length)]; } }
 
         public static void Load(ContentManager content)
         {
-            Music = content.Load<Song>("Sound/Castle Crashers - Jumper");
+            Music = content.Load<Song>("Sound/Castle Crashers - Jumper"); // DEV: This is just a debug song and it should be replaced
+            //SEffect = content.Load<SoundEffect>("");
+            // DEV: Or for sound effects with variation
+            // sEffects = Enumerable.Range(1, #).Select(x => content.Load<SoundEffect>("" + x)).ToArray();
         }
     }
 }
