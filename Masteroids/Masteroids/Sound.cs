52930024e95bfece32890cd87e384c8e4e720087
Simon <casimonni@gmail.com>
2018-04-14 14:06:21 +0200

    Added Sound class with Load method

---
+++ Masteroids/Masteroids/Sound.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+    static class Sound
+    {
+        public static Song Music { get; private set; }
+
+        public static void Load(ContentManager content)
+        {
+            Music = content.Load<Song>("");
+        }
+    }
+}
