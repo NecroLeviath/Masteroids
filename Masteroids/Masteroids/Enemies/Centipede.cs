bdfc8f96ed372843539db751290badb5cd60b41b
Simon <casimonni@gmail.com>
2018-04-25 14:08:20 +0200

    Added secondary constuctor for body parts

--- Masteroids/Masteroids/Enemies/Centipede.cs
+++ Masteroids/Masteroids/Enemies/Centipede.cs
@@ -10,10 +10,24 @@ namespace Masteroids
 {
     class Centipede : Enemy
     {
+        Centipede parent;
+        bool isHead;
+
         public Centipede(Texture2D texture, Vector2 position, float speed, Viewport viewport)
             : base(texture, position, speed, viewport)
         {
+            this.texture = texture;
+            this.speed = speed;
+            isHead = true;
+        }
 
+        public Centipede(Texture2D texture, Vector2 position, float speed, Viewport viewport, Centipede parent)
+            : base(texture, position, speed, viewport)
+        {
+            this.texture = texture;
+            this.speed = speed;
+            this.parent = parent;
+            isHead = false;
         }
     }
 }
