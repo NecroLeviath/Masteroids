511ad58381827dc62bafc8617977d475186f5e21
Simon <casimonni@gmail.com>
2018-04-29 17:27:15 +0200

    Implemented smooth turning in Centipede

--- Masteroids/Masteroids/Enemies/Centipede.cs
+++ Masteroids/Masteroids/Enemies/Centipede.cs
@@ -15,9 +15,10 @@ namespace Masteroids
         bool isHead;
         public List<Vector2> Beacons { get; private set; }
 		float beaconTimer, moveTimer;
-		float beaconInterval = 0.2f, moveInterval = 1f;
+		float beaconInterval = 0.2f, moveInterval = 3f;
+		float maxTurnRate = 2f;
 		Random rand = new Random();
-		int hp = 15; // DEV: Should be moved to constuctor
+		int hp = 1; // DEV: Should be moved to constuctor
 		Vector2 goal;
 
         public Centipede(Texture2D texture, Vector2 position, float speed, Viewport viewport, EntityManager entityMgr)
@@ -29,6 +30,7 @@ namespace Masteroids
             isHead = true;
 			Radius = texture.Height / 2;
             Beacons = new List<Vector2>();
+			FindGoal(moveInterval);
         }
 
         public Centipede(Texture2D texture, Vector2 position, float speed, Viewport viewport, Centipede parent, EntityManager entityMgr)
@@ -48,7 +50,10 @@ namespace Masteroids
             var delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (!isHead && !parent.IsAlive)
-                isHead = true;
+			{
+				isHead = true;
+				FindGoal(moveInterval);
+			}
 			if (hp < 0)
 				IsAlive = false;
 
@@ -63,21 +68,13 @@ namespace Masteroids
 
             if (isHead)
             {
-				moveTimer += delta;
-				if (moveTimer >= moveInterval || Vector2.Distance(position, goal) < 10)
-				{
-					float x = rand.Next(viewport.Width);
-					float y = rand.Next(viewport.Height);
-					goal = new Vector2(x, y);
-
-					moveTimer = 0;
-				}
-				direction = Vector2.Normalize(goal - position);
+				FindGoal(delta);
+				ClampAngle(delta);
 				velocity = direction * speed;
             }
             else if (!isHead && parent.Beacons.Count > 0)
             {
-                var distanceToBeacon = (parent.Beacons[0] - position);
+                Vector2 distanceToBeacon = (parent.Beacons[0] - position);
                 if (distanceToBeacon.LengthSquared() < speed * speed) // LengthSquared and speed^2 for efficiency
                 {
                     parent.RemoveFirstBeacon();
@@ -94,7 +91,7 @@ namespace Masteroids
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-			Vector2 drawPos = position - new Vector2(texture.Width / 2, texture.Height / 2);
+			var drawPos = position - new Vector2(texture.Width / 2, texture.Height / 2);
             spriteBatch.Draw(texture, drawPos, Color.White);
         }
 
@@ -107,6 +104,28 @@ namespace Masteroids
 		{
 			if (other is Bullet)
 				hp -= (other as Bullet).Damage;
+		}
+
+		private void FindGoal(float delta)
+		{
+			moveTimer += delta;
+			if (moveTimer >= moveInterval || Vector2.Distance(position, goal) < 10)
+			{
+				float x = rand.Next(200, viewport.Width - 200);
+				float y = rand.Next(200, viewport.Height - 200);
+				goal = new Vector2(x, y);
+
+				moveTimer = 0;
+			}
+		}
+
+		private void ClampAngle(float delta)
+		{
+			var currentAngle = (float)Math.Atan2(direction.Y, direction.X);
+			var desiredDirection = Vector2.Normalize(goal - position);
+			var desiredDirectionAngle = (float)Math.Atan2(desiredDirection.Y, desiredDirection.X);
+			currentAngle = MathHelper.Clamp(MathHelper.WrapAngle(desiredDirectionAngle - currentAngle), -maxTurnRate, maxTurnRate) * delta + currentAngle;
+			direction = new Vector2((float)Math.Cos(currentAngle), (float)Math.Sin(currentAngle));
 		}
 	}
 }
