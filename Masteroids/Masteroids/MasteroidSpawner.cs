6ad133dfd22e5577d2aa404e283b284c346372e3
Simon <casimonni@gmail.com>
2018-05-03 12:23:07 +0200

    Added MasteroidSpawner class and implemented its constructor

---
+++ Masteroids/Masteroids/MasteroidSpawner.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Masteroids
+{
+    class MasteroidSpawner : Spawner
+    {
+        private BaseBoss boss;
+
+        public MasteroidSpawner(EntityManager entityManager, Viewport viewport, BaseBoss boss)
+            : base(entityManager, viewport)
+        {
+            this.boss = boss;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+    }
+}
