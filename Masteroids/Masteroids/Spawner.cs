a370b23af7e6586b7a9d381b4ebec6cd8bc7d78f
Simon <casimonni@gmail.com>
2018-05-03 12:16:29 +0200

    Added abstract Spawner class and implemented its constructor and method

---
+++ Masteroids/Masteroids/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+    abstract class Spawner
+    {
+        EntityManager entityMgr;
+        Viewport viewport;
+
+        public Spawner(EntityManager entityManager, Viewport viewport)
+        {
+            entityMgr = entityManager;
+            this.viewport = viewport;
+        }
+
+        public abstract void Update(GameTime gameTime);
+    }
+}
