abfe89bc6b288e99f7b41ccc8f54ca32630dd7a2
Simon <casimonni@gmail.com>
2018-05-05 13:22:04 +0200

    Added and implemented Split method

--- Masteroids/Masteroids/Asteroid.cs
+++ Masteroids/Masteroids/Asteroid.cs
@@ -8,13 +8,13 @@ using System.Threading.Tasks;
 
 namespace Masteroids //Laila
 {
-    public class Asteroid : GameObject
-    {
-        Vector2 texOffset;
-        EntityManager entityMgr;
-        float HP;
+	public class Asteroid : GameObject
+	{
+		Vector2 texOffset;
+		EntityManager entityMgr;
+		float HP;
 		int size;
-        private int damage;
+		private int damage;
 		public int Damage //ANDREAS SOM FIFFLAT TILL 1 damage till ASTEROIDERNA
 		{
 			get { return damage; }
@@ -22,18 +22,18 @@ namespace Masteroids //Laila
 
 		public Asteroid(Texture2D texture, Vector2 speed, Vector2 position, Viewport viewport)
 			: base(texture, position, viewport)
-        {
-            tex = texture;
-            pos = position;
-            velocity = speed;
-            shouldWrap = true;
-            texOffset = new Vector2(tex.Width / 2, tex.Height / 2);
-            sourceRectangle = new Rectangle(0, 0, tex.Width, tex.Height);
+		{
+			tex = texture;
+			pos = position;
+			velocity = speed;
+			shouldWrap = true;
+			texOffset = new Vector2(tex.Width / 2, tex.Height / 2);
+			sourceRectangle = new Rectangle(0, 0, tex.Width, tex.Height);
 			Radius = tex.Width / 2;
-            damage = 1; //ANDREAS SOM FIFFLAT TILL 1 damage till ASTEROIDERNA
-            HP = 1;
+			damage = 1; //ANDREAS SOM FIFFLAT TILL 1 damage till ASTEROIDERNA
+			HP = 1;
 			size = 3;
-        }
+		}
 
 		public Asteroid(Texture2D texture, Vector2 position, Vector2 direction, float speed, int size, Viewport viewport)
 			: base(texture, position, viewport)
@@ -47,46 +47,70 @@ namespace Masteroids //Laila
 			damage = 1;
 			HP = 1;
 			shouldWrap = true;
+			velocity = direction * speed;
 		}
 
-        public override void Update(GameTime gameTime)
-        {
-            pos = pos + velocity;
-            ScreenWrap();
-        }
+		public override void Update(GameTime gameTime)
+		{
+			pos = pos + velocity;
 
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            if (HP == 1)
-            {
-                spriteBatch.Draw(tex, pos - texOffset, Color.White);
-                base.Draw(spriteBatch);
-            }
-        }
+			if (HP <= 0)
+			{
+				if (size > 1)
+				{
+					Split();
+				}
+				IsAlive = false;
+			}
 
-        protected override void WrapDraw(SpriteBatch spriteBatch)
-        {
-            if(HP == 1)
-            spriteBatch.Draw(tex, pos - texOffset + wrapOffset, Color.White);
-        }
-		
+			ScreenWrap();
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			if (HP == 1)
+			{
+				spriteBatch.Draw(tex, pos - texOffset, Color.White);
+				base.Draw(spriteBatch);
+			}
+		}
+
+		protected override void WrapDraw(SpriteBatch spriteBatch)
+		{
+			if (HP == 1)
+				spriteBatch.Draw(tex, pos - texOffset + wrapOffset, Color.White);
+		}
+
 		public override void HandleCollision(GameObject other)
 		{
 			if (other is Bullet)
 			{
 				HP -= (other as Bullet).Damage;
-				IsAlive = false;
-				//entityMgr.Asteroids.Count -= 1;
 			}
 			else if (other is Player)
 			{
-				IsAlive = false;
+				HP = 0;
 			}
 		}
 
 		public void Split()
 		{
-
+			var newTex = tex;
+			if (size == 3)
+				newTex = Art.AsteroidTexs[1];
+			else if (size == 2)
+				newTex = Art.AsteroidTexs[0];
+			direction = Vector2.Normalize(velocity);
+			rotation = (float)Math.Atan2(direction.Y, direction.X);
+			var newRotation = MathHelper.WrapAngle(rotation + 0.4f);
+			var newDirection = new Vector2((float)Math.Cos(newRotation), (float)Math.Sin(newRotation));
+			var newSpeed = velocity.Length();
+			Asteroid asteroid = new Asteroid(newTex, pos, direction, newSpeed, size--, viewport);
+			entityMgr.Add(asteroid);
+			newRotation = MathHelper.WrapAngle(rotation - 0.4f);
+			newDirection = new Vector2((float)Math.Cos(newRotation), (float)Math.Sin(newRotation));
+			asteroid = new Asteroid(newTex, pos, newDirection, newSpeed, size--, viewport);
+			entityMgr.Add(asteroid);
 		}
 	}
 }
