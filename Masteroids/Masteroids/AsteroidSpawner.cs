31598adc25b6cb360f2640a3665f7a151ff975d8
Simon <casimonni@gmail.com>
2018-05-05 13:21:28 +0200

    Replaced references to AsteroidTex with AsteroidTexs[2]

--- Masteroids/Masteroids/AsteroidSpawner.cs
+++ Masteroids/Masteroids/AsteroidSpawner.cs
@@ -18,13 +18,13 @@ namespace Masteroids
             //how many asteroids that spawns and how their movement is.
             if (entityMgr.Asteroids.Count < 10)
             {
-                int positionX = rnd.Next(0, viewport.Width - Art.AsteroidTex.Width);
-                int positionY = rnd.Next(0, viewport.Height + Art.AsteroidTex.Height);
+                int positionX = rnd.Next(0, viewport.Width - Art.AsteroidTexs[2].Width);
+                int positionY = rnd.Next(0, viewport.Height + Art.AsteroidTexs[2].Height);
                 int speedX = rnd.Next(-2, 2);   //Asteroider kan spawna utan att kunna röra sig.
                 int speedY = rnd.Next(-2, 2);
                 Vector2 pos = new Vector2(positionX, positionY);
                 Vector2 speed = new Vector2(speedX, speedY);
-				Asteroid asteroid = new Asteroid(Art.AsteroidTex, speed, pos, viewport);
+				Asteroid asteroid = new Asteroid(Art.AsteroidTexs[2], speed, pos, viewport);
 				entityMgr.Add(asteroid);
             }
             
