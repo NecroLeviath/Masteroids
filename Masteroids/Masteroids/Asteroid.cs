24b6a177883efb75b04c9484ae723d37d1699543
Simon <casimonni@gmail.com>
2018-05-05 12:59:17 +0200

    Added secondary constuctor to Asteroid

--- Masteroids/Masteroids/Asteroid.cs
+++ Masteroids/Masteroids/Asteroid.cs
@@ -13,9 +13,14 @@ namespace Masteroids //Laila
         Vector2 texOffset;
         EntityManager entityMgr;
         float HP;
+		int size;
         private int damage;
+		public int Damage //ANDREAS SOM FIFFLAT TILL 1 damage till ASTEROIDERNA
+		{
+			get { return damage; }
+		}
 
-        public Asteroid(Texture2D texture, Vector2 speed, Vector2 position, Viewport viewport)
+		public Asteroid(Texture2D texture, Vector2 speed, Vector2 position, Viewport viewport)
 			: base(texture, position, viewport)
         {
             tex = texture;
@@ -27,27 +32,29 @@ namespace Masteroids //Laila
 			Radius = tex.Width / 2;
             damage = 1; //ANDREAS SOM FIFFLAT TILL 1 damage till ASTEROIDERNA
             HP = 1;
-        
+			size = 3;
         }
+
+		public Asteroid(Texture2D texture, Vector2 position, Vector2 direction, float speed, int size, Viewport viewport)
+			: base(texture, position, viewport)
+		{
+			this.direction = direction;
+			this.speed = speed;
+			this.size = size;
+			texOffset = new Vector2(tex.Width / 2, tex.Height / 2);
+			sourceRectangle = new Rectangle(0, 0, tex.Width, tex.Height);
+			Radius = tex.Width / 2;
+			damage = 1;
+			HP = 1;
+			shouldWrap = true;
+		}
 
         public override void Update(GameTime gameTime)
         {
             pos = pos + velocity;
             ScreenWrap();
         }
-        public override void HandleCollision(GameObject other)
-        {
-            if (other is Bullet)
-            {
-                HP -= (other as Bullet).Damage;
-                IsAlive = false;
-                //entityMgr.Asteroids.Count -= 1;
-            }
-            if (other is Player)
-            {
-                IsAlive = false;
-            }
-        }
+
         public override void Draw(SpriteBatch spriteBatch)
         {
             if (HP == 1)
@@ -57,15 +64,29 @@ namespace Masteroids //Laila
             }
         }
 
-        public int Damage //ANDREAS SOM FIFFLAT TILL 1 damage till ASTEROIDERNA
-        {
-            get { return damage; }
-        }
-
         protected override void WrapDraw(SpriteBatch spriteBatch)
         {
             if(HP == 1)
             spriteBatch.Draw(tex, pos - texOffset + wrapOffset, Color.White);
         }
+		
+		public override void HandleCollision(GameObject other)
+		{
+			if (other is Bullet)
+			{
+				HP -= (other as Bullet).Damage;
+				IsAlive = false;
+				//entityMgr.Asteroids.Count -= 1;
+			}
+			else if (other is Player)
+			{
+				IsAlive = false;
+			}
+		}
+
+		public void Split()
+		{
+
+		}
 	}
 }
