41673710b5e5d951e0ebe78fbf9ae6be0498a18a
Simon <casimonni@gmail.com>
2018-05-12 16:14:32 +0200

    Added PlayerHandler class with initial constructor

---
+++ Masteroids/Masteroids/PlayerHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masteroids
+{
+	class PlayerHandler
+	{
+		PlayerIndex playerIndex;
+		EntityManager entityMgr;
+		Viewport viewport;
+		Vector2 spawnPos;
+
+		public PlayerHandler(PlayerIndex playerIndex, EntityManager entityManager, Viewport viewport)
+		{
+			this.playerIndex = playerIndex;
+			entityMgr = entityManager;
+			this.viewport = viewport;
+			spawnPos = new Vector2(viewport.Width / 2, viewport.Height / 2);
+		}
+	}
+}
