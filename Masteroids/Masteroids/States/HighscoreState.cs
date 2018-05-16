8264268993944a8d540d3d1e38d1114dc3dcc259
y8gi <36210065+y8gi@users.noreply.github.com>
2018-05-16 11:54:01 +0200

    Game mode
    Select Game mode
    added:
    NewGameState class
    HighScoreState class

--- Masteroids/Masteroids/States/HighscoreState.cs
+++ Masteroids/Masteroids/States/HighscoreState.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Masteroids.States
 {
-    class HighscoreState
+    class HighScoreState : State
     {
+        public HighScoreState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+
+
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Masteroids/Masteroids/States/NewGameState.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Masteroids.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Masteroids.States
+{
+    class NewGameState : State
+    {
+        private List<Component> _components;
+        private EntityManager entityMgr;
+        private BaseBoss boss;
+
+        public NewGameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            Texture2D buttonTexture = content.Load<Texture2D>("button");
+            SpriteFont buttonFont = content.Load<SpriteFont>("Fonts/MenuFont");
+            Sound.Load(content);
+            Sound.MusicInstance.Play();
+
+            int x = graphicsDevice.Viewport.Width;
+            int y = graphicsDevice.Viewport.Height;
+            Button ClassicGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2((x - buttonTexture.Width) / 2, 625),
+                Text = "Classic Mode"
+            };
+
+            Button MasteroidsGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2((x - buttonTexture.Width) / 2, 675),
+                Text = "Masteroid Mode"
+            };
+
+            ClassicGameButton.Click += ClassicGameButton_click;
+            MasteroidsGameButton.Click += MasteroidsGameButton_click;
+            _components = new List<Component>()
+            {
+                ClassicGameButton,
+                MasteroidsGameButton,
+
+            };
+        }
+        private void ClassicGameButton_click(object sender, EventArgs e)
+        {
+            game.ChangeState(new GameState(game, graphicsDevice, content, entityMgr, 1));
+        }
+
+        private void MasteroidsGameButton_click(object sender, EventArgs e)
+        {
+            boss = new Centipede(Art.CentipedeSheet, new Vector2(200), 240, 3, 99, graphicsDevice.Viewport, entityMgr);
+            game.ChangeState(new GameState(game, graphicsDevice, content, entityMgr, 1, boss));
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (Masteroids.Component component in _components)
+                component.Draw(gameTime, spriteBatch);
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
