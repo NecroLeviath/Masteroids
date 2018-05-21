e722ca10eec32510ed04105c2fce2cd7ccb11449
Simon <casimonni@gmail.com>
2018-05-21 17:00:43 +0200

    Removed out-commented code and font from GameState

--- Masteroids/Masteroids/States/GameState.cs
+++ Masteroids/Masteroids/States/GameState.cs
@@ -12,16 +12,10 @@ namespace Masteroids
 {
 	public class GameState : State
 	{
-		SpriteFont font;
 		Viewport viewport;
-
 		EntityManager entityMgr;
 		Spawner spawner;
 		List<PlayerHandler> playerHandlers;
-
-		//Vector2 bossFontPos;
-		//Vector2 bossFontPos;
-		//SpriteFont bossFont;
 
 		// Asteroids
 		public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, EntityManager entityManager, int numberOfPlayers)
@@ -44,9 +38,6 @@ namespace Masteroids
 			viewport = graphicsDevice.Viewport;
 			entityMgr = entityManager;
 			playerHandlers = new List<PlayerHandler>();
-			//bossFont = content.Load<SpriteFont>("BossLife");
-			//bossFontPos = new Vector2(1000, 20);
-			font = content.Load<SpriteFont>(@"Fonts/font");
 
 			PlayerIndex[] players = new PlayerIndex[]
 			{
@@ -66,8 +57,6 @@ namespace Masteroids
 			{
 				PlayerHandler playerHandler = new PlayerHandler(players[i], phDrawPos[i], Assets.ButtonFont, entityMgr, viewport);
 				playerHandlers.Add(playerHandler);
-				//Player player = new Player(Art.PlayerTex, new Vector2(viewport.Width / 2, viewport.Height / 2), players[i], entityMgr, viewport);
-				//entityMgr.Add(player);
 			}
 		}
 
@@ -77,26 +66,6 @@ namespace Masteroids
 			entityMgr.Update(gameTime);
 			foreach (PlayerHandler ph in playerHandlers)
 				ph.Update(gameTime);
-			#region Out-commented
-			//GamePadCapabilities capabilities =
-			//    GamePad.GetCapabilities(PlayerIndex.Two);
-			//GamePadCapabilities capabilities = GamePad.GetCapabilities(PlayerIndex.One);
-			//if (capabilities.IsConnected)
-			//{
-			//    GamePadState gamePadState = GamePad.GetState(PlayerIndex.Two);
-			//    //if (capabilities.HasLeftXThumbStick)
-			//    //Player 2, Tangentboard endast
-			//    if (gamePadState.Buttons.Start == ButtonState.Pressed)
-			//    {
-			//        enteredGame = true;
-			//    }
-			//}
-
-			//if (enteredGame)
-			//{
-			//    player2.Update(gameTime);
-			//}
-			#endregion
 		}
 
 		public override void PostUpdate(GameTime gameTime) { }
@@ -110,25 +79,13 @@ namespace Masteroids
 			if (entityMgr.Bosses.Count > 0)
 			{
 				int bossHP = 0;
-				for (int i = 0; i < entityMgr.Bosses.Count; i++)    // Summarizes the HP of the boss.
+				for (int i = 0; i < entityMgr.Bosses.Count; i++) // Summarizes the HP of the boss.
 					bossHP += entityMgr.Bosses[i].HP;
-				//spriteBatch.DrawString(bossFont, "Life: " + bossHP + " / " + (spawner as MasteroidSpawner).BossMaxHP, bossFontPos, Color.White);
 				Rectangle bossHPRect = new Rectangle(((viewport.Width - 300) / 2), 50, 300, 10);
 				spriteBatch.Draw(Assets.CentipedeTex, bossHPRect, new Rectangle(30, 30, 1, 1), Color.DarkRed);
 				bossHPRect.Width = bossHPRect.Width * bossHP / (spawner as MasteroidSpawner).BossMaxHP;
 				spriteBatch.Draw(Assets.CentipedeTex, bossHPRect, new Rectangle(30, 30, 1, 1), Color.Red);
 			}
-
-			#region Out-commented
-			//if (enteredGame)
-			//{
-			//    player2.Draw(spriteBatch);
-			//}
-			//else
-			//{
-			//    spriteBatch.DrawString(font, "Press start to Enter", new Vector2(1700, 980), Color.White);
-			//}
-			#endregion
 		}
 	}
 }
